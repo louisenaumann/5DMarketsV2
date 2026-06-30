891fa61368a5430b98d330e938faef533c115ac1
Louise Naumann <44986952+louisenaumann@users.noreply.github.com>
2026-07-01 01:12:29 +0200

    Create future_transition.py

---
+++ src/layers/future_transition.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+
+from config import *
+
+
+class FutureTransition(nn.Module):
+
+    """
+    Φ operator
+
+    Predict future market trajectory
+    from historical market state
+    """
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.project = nn.Sequential(
+
+            nn.Linear(
+
+                LATENT_DIM,
+
+                128
+
+            ),
+
+            nn.ReLU(),
+
+            nn.Linear(
+
+                128,
+
+                LATENT_DIM * TRAJECTORY_LENGTH
+
+            )
+
+        )
+
+    def forward(self,
+                x):
+
+        #
+        # input:
+        #
+        # [batch, latent_dim]
+        #
+
+        output = self.project(
+            x
+        )
+
+        #
+        # reshape
+        #
+
+        output = output.view(
+
+            -1,
+
+            TRAJECTORY_LENGTH,
+
+            LATENT_DIM
+
+        )
+
+        return output
